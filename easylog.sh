edc8189569774c1bec727d12f72703ff4c67541f
Jason Regula <58050904+Jregula-pcsd@users.noreply.github.com>
2020-05-22 16:15:53 -0800

    Version 0.1.0!
    Added support for a log EOF (footer).
    Added sample for using a veriable to hold your EasyLOG script configuration.
    Added better documention on latest changes.
    
    Co-Authored-By: rayregula <rayregula@users.noreply.github.com>

--- easylog.sh
+++ easylog.sh
@@ -3,7 +3,7 @@
 ## by defining which arguments to print and then calling the script
 ## v0.0.8 By Jason Regula
 
-VERSION="v0.0.9"
+VERSION="v0.1.0"
 CURRENTDATE=$(date "+%m-%d-%Y") # current date
 CURRENTTIME=$(date "+%I:%M %p") # current time 12 hour clock
 CURRENTTIME24=$(date "+%H:%M") # current time 24 hour clock
@@ -11,6 +11,11 @@ EasyLOG_FILENAME=${EasyLOG_FILENAME:-"$EasyLOG_SCRIPT_NAME"\_"$CURRENTDATE.txt"}
 EasyLOG_DIR=${EasyLOG_DIR:-$HOME/Documents/LOGS/}
 EasyLOG_SCRIPT_NAME=${EasyLOG_SCRIPT_NAME:-"EasyLOG.sh"}
 EasyLOG_SCRIPT_NAMEINFO=${EasyLOG_SCRIPT_NAMEINFO:-""}
+USE24HOUR=${USE24HOUR:-"false"}
+[ $USE24HOUR == true ] && CURRENTTIME=$CURRENTTIME24
+SHOW_EasyLOG_END=${SHOW_EasyLOG_END:-"true"}
+
+
 clear
 echo ""
 echo "EasyLOG.sh -- create easy log files"
@@ -30,27 +35,38 @@ EasyLOG_HEADER=${EasyLOG_HEADER:-"\
 ################################# \n\
 "}
 
-EasyLOG_DATA=${EasyLOG_DATA:-"\
+# defualt log datablock
+EasyLOG_DATABLOCK=${EasyLOG_DATABLOCK:-"\
 # Overwrite values in your own script. end lines with <backslash n backslash> \n\
 # \n\
-# From within your script you can use: <EasyLOG_DATA=\$CUSTOM_FUNCTION> ./easylog.sh \n\
+# From within your script you can use: <EasyLOG_DATABLOCK=\$CUSTOM_FUNCTION> ./easylog.sh \n\
 # for any of the veriables below to set yoru own values. \n\
 # \n\
 # EasyLOG_NAME=Log_\$CURRENTDATE.txt #Name the log file will be saved with. \n\
 # EasyLOG_DIR=\$HOME/Documents/LOGS/ #Location files will be created. \n\
 # EasyLOG_SCRIPT_NAME=\${EasyLOG_SCRIPT_NAME:-EasyLOG.sh} #name of script that the log file was created by. \n\
 # EasyLOG_HEADER= #how the top of the log file will appear. \n\
-# EasyLOG_DATA= #the informaition to be entered in the body of the log file. \n\
+# EasyLOG_DATABLOCK= #the informaition to be entered in the body of the log file. \n\
 # See the README here: https://github.com/Rayregula/EasyLOG/blob/master/README.md
 "}
 
 
-EasyLOG_DATA2=${EasyLOG_DATA2:-""}
+EasyLOG_DATABLOCK2=${EasyLOG_DATABLOCK2:-" "}
 
 
-EasyLOG_DATA3=${EasyLOG_DATA3:-""}
+EasyLOG_DATABLOCK3=${EasyLOG_DATABLOCK3:-" "}
 
 
+EasyLOG_END=${EasyLOG_END:-"\
+## End of Log File \n\
+## Check out the EasyLOG Project on Github <3 \n\
+## https://github.com/Rayregula/EasyLOG
+"}
+
+## clear EasyLOG_END if SHOW_EasyLOG_END is set to false
+[ $SHOW_EasyLOG_END == false ] && EasyLOG_END=""
+
+## you can reformat the log files here
 function LOG_FORMAT() {
   echo $EasyLOG_HEADER
   echo ""
@@ -59,6 +75,9 @@ function LOG_FORMAT() {
   echo $EasyLOG_DATA2
   echo ""
   echo $EasyLOG_DATA3
+  echo ""
+  echo ""
+  echo $EasyLOG_END
 
 }
 
@@ -67,7 +86,7 @@ function CREATE_LOG() {
 }
 
 echo ""
-echo "Log file created at $EasyLOG_DIR with name $EasyLOG_FILENAME"
+echo "Log file created at $EasyLOG_DIR with name: $EasyLOG_FILENAME"
 
 CREATE_LOG
 exit 0
--- sample_script_advanced.sh
+++ sample_script_advanced.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 ## advanced_sample script for EasyLOG.sh
-## version 0.0.3 By Jason Regula
+## version 0.0.4 By Jason Regula
 
+VERSION="0.0.4"
 SCRIPT_NAME="sample_script_advanced.sh"
 DIRFORLOGS="$HOME/Documents/myeasylogs/"
-#EasyLOG_FILENAME=""
+
 ## Config for log output.
 LOGFILE_DATA="\
 This is a sample log file \n\
@@ -12,6 +13,7 @@ This is a sample log file \n\
 Current User is $USER \n\
 The script working Directory is: $(pwd) \n\
 "
+
 ## Config for log block 2 output
 LOGFILE_DATA2="\
 You can pass data from existing variables \n\
@@ -21,5 +23,20 @@ plain data may also be assigned directly to an EasyLOG variable if it's easier t
 \n\
 <3 Jason \n\
 "
+
 ## Overwrite EasyLOG built-in values and call script.
-EasyLOG_SCRIPT_NAME=$SCRIPT_NAME EasyLOG_DIR=$DIRFORLOGS EasyLOG_DATA=$LOGFILE_DATA EasyLOG_DATA2=$LOGFILE_DATA2 EasyLOG_DATA3="current time/date is: $(date)" ./EasyLOG.sh
+## if you have a bunch of things to override it makes it a lot cleaner to escape your newlines.
+## then you can easily see all your options
+## be sure to place a space before or after the escape character so they don't end up as a single string
+CONFIG_FOR_EasyLOG= \
+EasyLOG_SCRIPT_NAME=$SCRIPT_NAME \
+USE24HOUR=true \
+EasyLOG_SCRIPT_NAMEINFO="(version: $VERSION)" \
+EasyLOG_DIR=$DIRFORLOGS \
+EasyLOG_DATA=$LOGFILE_DATA \
+EasyLOG_DATA2=$LOGFILE_DATA2 \
+EasyLOG_DATA3="current time/date is: $(date)" \
+./EasyLOG.sh
+
+## run veriable
+$CONFIG_FOR_EasyLOG
